20d95f41a565c41dc72f861e984cc7426a0d14f2
Tom Scheler <tom.scheler@web.de>
2023-03-12 00:25:13 +0100

    Add canvas support

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,38 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Browser
+import Canvas exposing (..)
+import Canvas.Settings exposing (..)
+import Canvas.Settings.Advanced exposing (..)
+import Color
+import Html exposing (Html)
 
 
-main : Html.Html msg
+main : Program () Model Msg
 main =
-    text "Hello there!"
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Model =
+    ()
+
+
+init : Model
+init =
+    ()
+
+
+type alias Msg =
+    ()
+
+
+update : Msg -> Model -> Model
+update _ model =
+    model
+
+
+view : Model -> Html Msg
+view _ =
+    toHtml ( 500, 500 )
+        []
+        [ shapes [ fill Color.red ] [ rect ( 0, 0 ) 500 500 ] ]
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { Elm } from "./Main.elm";
+import 'elm-canvas';
 
 Elm.Main.init({ node: document.getElementById("root") });
