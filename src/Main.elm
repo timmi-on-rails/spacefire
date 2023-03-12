cb5f93c4ba2bd268673a53c3ad0a7d60e6d8ea2a
Tom Scheler <tom.scheler@web.de>
2023-03-12 11:47:41 +0100

    Handle key navigation

--- src/Main.elm
+++ src/Main.elm
@@ -1,38 +1,82 @@
 module Main exposing (..)
 
 import Browser
+import Browser.Events exposing (onAnimationFrameDelta)
 import Canvas exposing (..)
 import Canvas.Settings exposing (..)
 import Canvas.Settings.Advanced exposing (..)
 import Color
 import Html exposing (Html)
+import Keyboard exposing (Key(..), RawKey)
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 type alias Model =
-    ()
+    { x : Float
+    , vx : Float
+    , pressedKeys : List Key
+    }
 
 
-init : Model
-init =
-    ()
+init : () -> ( Model, Cmd msg )
+init _ =
+    ( { x = 0
+      , vx = 0
+      , pressedKeys = []
+      }
+    , Cmd.none
+    )
 
 
-type alias Msg =
-    ()
+type Msg
+    = KeyDown RawKey
+    | Tick Float
 
 
-update : Msg -> Model -> Model
-update _ model =
-    model
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        KeyDown k ->
+            case Keyboard.anyKeyOriginal k of
+                Just ArrowLeft ->
+                    ( { model | vx = -0.1 }, Cmd.none )
+
+                Just ArrowRight ->
+                    ( { model | vx = 0.1 }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        Tick delta ->
+            ( { model | x = model.x + model.vx * delta }, Cmd.none )
 
 
 view : Model -> Html Msg
-view _ =
+view model =
     toHtml ( 500, 500 )
         []
-        [ shapes [ fill Color.black ] [ rect ( 0, 0 ) 500 500 ] ]
+        (shapes [ fill Color.black ] [ rect ( 0, 0 ) 500 500 ]
+            :: renderItems model
+        )
+
+
+renderItems : Model -> List Renderable
+renderItems model =
+    [ shapes [ fill Color.red ] [ rect ( model.x - 25, 400 ) 50 50 ] ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch
+        [ onAnimationFrameDelta Tick
+        , Keyboard.downs KeyDown
+        ]
