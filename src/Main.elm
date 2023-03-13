dacc607584eed3c9417b5fe6b036e3788cc4896c
Tom Scheler <tom.scheler@web.de>
2023-03-13 23:58:15 +0100

    Randomly add moving fire balls

--- src/Main.elm
+++ src/Main.elm
@@ -8,6 +8,7 @@ import Canvas.Settings.Advanced exposing (..)
 import Color
 import Html exposing (Html)
 import Keyboard exposing (Key(..))
+import Random
 
 
 main : Program () Model Msg
@@ -33,13 +34,21 @@ type alias Ship =
     }
 
 
+
+-- TODO: Threading seeds around is not super fun, so if you really need this, it is best to build your Generator like normal and then just step it all at once at the top of your program.
+
+
 type alias Model =
     { ship : Ship
+    , seed : Random.Seed
+    , nextFireEta : Float
+    , fires : List ( Float, Float )
+    , missedFires : Int
     , pressedKeys : List Key
     }
 
 
-init : () -> ( Model, Cmd msg )
+init : () -> ( Model, Cmd Msg )
 init _ =
     ( { ship =
             { x = 0
@@ -47,15 +56,20 @@ init _ =
             , width = 50
             , height = 50
             }
+      , seed = Random.initialSeed 0
+      , nextFireEta = 0
+      , fires = []
+      , missedFires = 0
       , pressedKeys = []
       }
-    , Cmd.none
+    , Random.generate InitSeed Random.independentSeed
     )
 
 
 type Msg
     = KeyMsg Keyboard.Msg
     | Tick Float
+    | InitSeed Random.Seed
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -93,23 +107,105 @@ update msg model =
             )
 
         Tick delta ->
-            ( { model
-                | ship =
-                    let
-                        ship =
-                            model.ship
-                    in
-                    { ship
-                        | x =
-                            let
-                                ( w, _ ) =
-                                    canvasSize
-                            in
-                            max (-0.5 * w + 0.5 * ship.width) (min (0.5 * w - 0.5 * ship.width) (ship.x + ship.vx * delta))
-                    }
-              }
+            ( model
+                |> moveShip delta
+                |> decrementFireEta delta
+                |> spawnFire
+                |> resetFireEta
+                |> moveFires delta
+                |> handleMissedFires
             , Cmd.none
             )
+
+        InitSeed s ->
+            ( { model | seed = s } |> resetFireEta, Cmd.none )
+
+
+moveFires : Float -> Model -> Model
+moveFires delta model =
+    let
+        move : ( Float, Float ) -> ( Float, Float )
+        move f =
+            ( Tuple.first f, Tuple.second f + 0.1 * delta )
+
+        newFires =
+            model.fires |> List.map move
+    in
+    { model | fires = newFires }
+
+
+moveShip : Float -> Model -> Model
+moveShip delta model =
+    { model
+        | ship =
+            let
+                ship =
+                    model.ship
+            in
+            { ship
+                | x =
+                    let
+                        ( w, _ ) =
+                            canvasSize
+                    in
+                    max (-0.5 * w + 0.5 * ship.width) (min (0.5 * w - 0.5 * ship.width) (ship.x + ship.vx * delta))
+            }
+    }
+
+
+resetFireEta : Model -> Model
+resetFireEta model =
+    if model.nextFireEta <= 0 then
+        let
+            g =
+                Random.float 1000 5000
+
+            ( r, newModel ) =
+                step g model
+        in
+        { newModel | nextFireEta = r }
+
+    else
+        model
+
+
+handleMissedFires : Model -> Model
+handleMissedFires model =
+    let
+        ( _, h ) =
+            canvasSize
+
+        cond f =
+            Tuple.second f < h
+
+        newFires =
+            model.fires |> List.filter cond
+    in
+    { model | fires = newFires, missedFires = List.length model.fires - List.length newFires }
+
+
+decrementFireEta : Float -> Model -> Model
+decrementFireEta delta model =
+    { model | nextFireEta = model.nextFireEta - delta }
+
+
+spawnFire : Model -> Model
+spawnFire model =
+    if model.nextFireEta <= 0 then
+        let
+            ( w, _ ) =
+                canvasSize
+
+            g =
+                Random.float (-0.5 * w) (0.5 * w)
+
+            ( r, newModel ) =
+                step g model
+        in
+        { newModel | fires = ( r, 0 ) :: model.fires }
+
+    else
+        model
 
 
 view : Model -> Html Msg
@@ -136,7 +232,31 @@ renderItems model =
           in
           rect ( 0.5 * w + model.ship.x - 0.5 * model.ship.width, 0.8 * h ) model.ship.width model.ship.height
         ]
+    , shapes [ fill Color.orange ]
+        (fireShapes model)
     ]
+
+
+fireShapes : Model -> List Shape
+fireShapes model =
+    let
+        ( w, _ ) =
+            canvasSize
+    in
+    model.fires
+        |> List.map
+            (\f ->
+                rect ( 0.5 * w + Tuple.first f - 0.5 * 10, Tuple.second f ) 10 10
+            )
+
+
+step : Random.Generator a -> Model -> ( a, Model )
+step generator model =
+    let
+        ( a, nextSeed ) =
+            model.seed |> Random.step generator
+    in
+    ( a, { model | seed = nextSeed } )
 
 
 subscriptions : Model -> Sub Msg
