55e9166db873ced2b500e5cb31da00747e2bf096
Tom Scheler <tom.scheler@web.de>
2023-03-25 22:52:31 +0100

    Add game initialization step

--- src/Main.elm
+++ src/Main.elm
@@ -46,53 +46,70 @@ type alias Ship =
 -- TODO: Threading seeds around is not super fun, so if you really need this, it is best to build your Generator like normal and then just step it all at once at the top of your program.
 
 
-type alias Model =
-    { ship : Ship
-    , seed : Random.Seed
+type Model
+    = Initializing EnvBuilder
+    | Initialized (Result String Env)
+    | Running Game
+
+
+type alias EnvBuilder =
+    { seed : Maybe Random.Seed
+    , shipTexture : Maybe Canvas.Texture.Texture
+    , fireTexture : Maybe Canvas.Texture.Texture
+    }
+
+
+type alias Env =
+    { seed : Random.Seed
+    , pressedKeys : List Keyboard.Key
+    , pressedButtons : List Button
+    , shipTexture : Canvas.Texture.Texture
+    , fireTexture : Canvas.Texture.Texture
+    }
+
+
+type alias Game =
+    { env : Env
+    , ship : Ship
     , nextFireEta : Float
     , fires : List ( Float, Float )
     , missedFires : Int
     , killedFires : Int
-    , pressedKeys : List Keyboard.Key
-    , pressedButtons : List Button
-    , shipTexture : Load Canvas.Texture.Texture
-    , fireTexture : Load Canvas.Texture.Texture
     }
-
-
-type Load a
-    = Loading
-    | Success a
-    | Failure
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { ship =
-            { x = 0
-            , y = 0.8 * Tuple.second canvasSize
-            , width = 50
-            , height = 50
-            }
-      , seed = Random.initialSeed 0
-      , nextFireEta = 0
-      , fires = []
-      , missedFires = 0
-      , killedFires = 0
-      , pressedKeys = []
-      , pressedButtons = []
-      , shipTexture = Loading
-      , fireTexture = Loading
-      }
+    ( Initializing
+        { seed = Nothing
+        , fireTexture = Nothing
+        , shipTexture = Nothing
+        }
     , Random.generate InitSeed Random.independentSeed
     )
+
+
+newGame : Env -> Game
+newGame env =
+    { env = env
+    , ship =
+        { x = 0
+        , y = 0.8 * Tuple.second canvasSize
+        , width = 50
+        , height = 50
+        }
+    , nextFireEta = 0
+    , fires = []
+    , missedFires = 0
+    , killedFires = 0
+    }
 
 
 type Msg
     = KeyMsg Keyboard.Msg
     | Tick Float
     | InitSeed Random.Seed
-    | TextureLoaded (Model -> Load Canvas.Texture.Texture -> Model) (Maybe Canvas.Texture.Texture)
+    | TextureLoaded String (EnvBuilder -> Canvas.Texture.Texture -> EnvBuilder) (Maybe Canvas.Texture.Texture)
     | MouseMsg (MouseMsg Button)
 
 
@@ -111,28 +128,123 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         KeyMsg k ->
-            ( { model | pressedKeys = Keyboard.update k model.pressedKeys }, Cmd.none )
+            case model of
+                Initialized (Ok env) ->
+                    ( tryRunGame { env | pressedKeys = Keyboard.update k env.pressedKeys }, Cmd.none )
+
+                Running game ->
+                    ( Running
+                        { game
+                            | env =
+                                let
+                                    env =
+                                        game.env
+                                in
+                                { env | pressedKeys = Keyboard.update k env.pressedKeys }
+                        }
+                    , Cmd.none
+                    )
+
+                _ ->
+                    ( model, Cmd.none )
 
         Tick delta ->
-            ( model
-                |> moveShip delta
-                |> decrementFireEta delta
-                |> spawnFire
-                |> resetFireEta
-                |> moveFires delta
-                |> handleMissedFires
-                |> handleCollisions
-            , Cmd.none
-            )
+            case model of
+                Running game ->
+                    ( Running
+                        (game
+                            |> moveShip delta
+                            |> decrementFireEta delta
+                            |> spawnFire
+                            |> resetFireEta
+                            |> moveFires delta
+                            |> handleMissedFires
+                            |> handleCollisions
+                        )
+                    , Cmd.none
+                    )
+
+                _ ->
+                    ( model, Cmd.none )
 
         InitSeed s ->
-            ( { model | seed = s } |> resetFireEta, Cmd.none )
+            case model of
+                Initializing envBuilder ->
+                    ( tryInitEnv { envBuilder | seed = Just s }, Cmd.none )
 
-        TextureLoaded k mt ->
-            ( loadTexture model k mt, Cmd.none )
+                _ ->
+                    ( model, Cmd.none )
+
+        TextureLoaded name u mt ->
+            case mt of
+                Just t ->
+                    case model of
+                        Initializing envBuilder ->
+                            ( tryInitEnv (u envBuilder t), Cmd.none )
+
+                        _ ->
+                            ( model, Cmd.none )
+
+                Nothing ->
+                    ( Err ("failed to load texture " ++ name) |> Initialized, Cmd.none )
 
         MouseMsg x ->
-            ( { model | pressedButtons = updateButtons x model.pressedButtons }, Cmd.none )
+            case model of
+                Initialized (Ok env) ->
+                    ( tryRunGame { env | pressedButtons = updateButtons x env.pressedButtons }, Cmd.none )
+
+                Running game ->
+                    ( Running
+                        { game
+                            | env =
+                                let
+                                    env =
+                                        game.env
+                                in
+                                { env | pressedButtons = updateButtons x env.pressedButtons }
+                        }
+                    , Cmd.none
+                    )
+
+                _ ->
+                    ( model, Cmd.none )
+
+
+tryRunGame : Env -> Model
+tryRunGame env =
+    if List.length env.pressedButtons > 0 || List.length env.pressedKeys > 0 then
+        newGame env |> resetFireEta |> Running
+
+    else
+        Ok env |> Initialized
+
+
+tryInitEnv : EnvBuilder -> Model
+tryInitEnv envBuilder =
+    case envBuilder.shipTexture of
+        Just shipTexture ->
+            case envBuilder.fireTexture of
+                Just fireTexture ->
+                    case envBuilder.seed of
+                        Just seed ->
+                            Initialized
+                                (Ok
+                                    { seed = seed
+                                    , fireTexture = fireTexture
+                                    , shipTexture = shipTexture
+                                    , pressedKeys = []
+                                    , pressedButtons = []
+                                    }
+                                )
+
+                        Nothing ->
+                            Initializing envBuilder
+
+                Nothing ->
+                    Initializing envBuilder
+
+        Nothing ->
+            Initializing envBuilder
 
 
 updateButtons : MouseMsg a -> List a -> List a
@@ -148,20 +260,20 @@ updateButtons mouseMsg buttons =
             x :: List.filter ((/=) x) buttons
 
 
-shipVx : Model -> Float
-shipVx model =
+shipVx : Game -> Float
+shipVx game =
     List.sum
         [ if
-            List.member Keyboard.ArrowLeft model.pressedKeys
-                || List.member Left model.pressedButtons
+            List.member Keyboard.ArrowLeft game.env.pressedKeys
+                || List.member Left game.env.pressedButtons
           then
             -0.3
 
           else
             0
         , if
-            List.member Keyboard.ArrowRight model.pressedKeys
-                || List.member Right model.pressedButtons
+            List.member Keyboard.ArrowRight game.env.pressedKeys
+                || List.member Right game.env.pressedButtons
           then
             0.3
 
@@ -170,36 +282,26 @@ shipVx model =
         ]
 
 
-loadTexture : Model -> (Model -> Load Canvas.Texture.Texture -> Model) -> Maybe Canvas.Texture.Texture -> Model
-loadTexture model k mt =
-    case mt of
-        Just t ->
-            k model (Success t)
-
-        Nothing ->
-            k model Failure
-
-
-moveFires : Float -> Model -> Model
-moveFires delta model =
+moveFires : Float -> Game -> Game
+moveFires delta game =
     let
         move : ( Float, Float ) -> ( Float, Float )
         move f =
             ( Tuple.first f, Tuple.second f + 0.1 * delta )
 
         newFires =
-            model.fires |> List.map move
+            game.fires |> List.map move
     in
-    { model | fires = newFires }
+    { game | fires = newFires }
 
 
-moveShip : Float -> Model -> Model
-moveShip delta model =
-    { model
+moveShip : Float -> Game -> Game
+moveShip delta game =
+    { game
         | ship =
             let
                 ship =
-                    model.ship
+                    game.ship
             in
             { ship
                 | x =
@@ -207,29 +309,29 @@ moveShip delta model =
                         ( w, _ ) =
                             canvasSize
                     in
-                    max (-0.5 * w + 0.5 * ship.width) (min (0.5 * w - 0.5 * ship.width) (ship.x + shipVx model * delta))
+                    max (-0.5 * w + 0.5 * ship.width) (min (0.5 * w - 0.5 * ship.width) (ship.x + shipVx game * delta))
             }
     }
 
 
-resetFireEta : Model -> Model
-resetFireEta model =
-    if model.nextFireEta <= 0 then
+resetFireEta : Game -> Game
+resetFireEta game =
+    if game.nextFireEta <= 0 then
         let
             g =
                 Random.float 1000 5000
 
-            ( r, newModel ) =
-                step g model
+            ( r, newEnv ) =
+                step g game.env
         in
-        { newModel | nextFireEta = r }
+        { game | env = newEnv, nextFireEta = r }
 
     else
-        model
+        game
 
 
-handleMissedFires : Model -> Model
-handleMissedFires model =
+handleMissedFires : Game -> Game
+handleMissedFires game =
     let
         ( _, h ) =
             canvasSize
@@ -238,13 +340,13 @@ handleMissedFires model =
             Tuple.second f < h
 
         newFires =
-            model.fires |> List.filter cond
+            game.fires |> List.filter cond
     in
-    { model | fires = newFires, missedFires = List.length model.fires - List.length newFires }
+    { game | fires = newFires, missedFires = List.length game.fires - List.length newFires }
 
 
-handleCollisions : Model -> Model
-handleCollisions model =
+handleCollisions : Game -> Game
+handleCollisions game =
     let
         ( fw, fh ) =
             fireSize
@@ -253,39 +355,39 @@ handleCollisions model =
             Tuple.second f
                 + 0.5
                 * fh
-                <= model.ship.y
+                <= game.ship.y
                 || Tuple.second f
                 - 0.5
                 * fh
-                >= model.ship.y
-                + model.ship.height
+                >= game.ship.y
+                + game.ship.height
                 || Tuple.first f
                 + 0.5
                 * fw
-                <= model.ship.x
+                <= game.ship.x
                 - 0.5
-                * model.ship.width
+                * game.ship.width
                 || Tuple.first f
                 - 0.5
                 * fw
-                >= model.ship.x
+                >= game.ship.x
                 + 0.5
-                * model.ship.width
+                * game.ship.width
 
         newFires =
-            model.fires |> List.filter noCollision
+            game.fires |> List.filter noCollision
     in
-    { model | fires = newFires, killedFires = List.length model.fires - List.length newFires }
+    { game | fires = newFires, killedFires = List.length game.fires - List.length newFires }
 
 
-decrementFireEta : Float -> Model -> Model
-decrementFireEta delta model =
-    { model | nextFireEta = model.nextFireEta - delta }
+decrementFireEta : Float -> Game -> Game
+decrementFireEta delta game =
+    { game | nextFireEta = game.nextFireEta - delta }
 
 
-spawnFire : Model -> Model
-spawnFire model =
-    if model.nextFireEta <= 0 then
+spawnFire : Game -> Game
+spawnFire game =
+    if game.nextFireEta <= 0 then
         let
             ( w, _ ) =
                 canvasSize
@@ -293,19 +395,19 @@ spawnFire model =
             g =
                 Random.float (-0.5 * w) (0.5 * w)
 
-            ( r, newModel ) =
-                step g model
+            ( r, newEnv ) =
+                step g game.env
         in
-        { newModel | fires = ( r, 0 ) :: model.fires }
+        { game | env = newEnv, fires = ( r, 0 ) :: game.fires }
 
     else
-        model
+        game
 
 
 textures : List (Canvas.Texture.Source Msg)
 textures =
-    [ Canvas.Texture.loadFromImageUrl "./ship.png" (TextureLoaded (\model load -> { model | shipTexture = load }))
-    , Canvas.Texture.loadFromImageUrl "./fire.png" (TextureLoaded (\model load -> { model | fireTexture = load }))
+    [ Canvas.Texture.loadFromImageUrl "./ship.png" (TextureLoaded "ship" (\envBuilder texture -> { envBuilder | shipTexture = Just texture }))
+    , Canvas.Texture.loadFromImageUrl "./fire.png" (TextureLoaded "fire" (\envBuilder texture -> { envBuilder | fireTexture = Just texture }))
     ]
 
 
@@ -343,44 +445,50 @@ mouseEvents f a =
 
 renderCanvas : Model -> Html.Html Msg
 renderCanvas model =
-    Canvas.toHtmlWith
-        { width = Tuple.first canvasSize
-        , height = Tuple.second canvasSize
-        , textures = textures
-        }
-        []
-        (Canvas.shapes [ Canvas.Settings.fill Color.black ]
-            [ let
-                ( w, h ) =
-                    canvasSize
-              in
-              Canvas.rect ( 0, 0 ) w h
-            ]
-            :: renderItems model
-        )
+    case model of
+        Initializing _ ->
+            Canvas.toHtmlWith
+                { width = Tuple.first canvasSize
+                , height = Tuple.second canvasSize
+                , textures = textures
+                }
+                []
+                [ Canvas.text [] ( 100, 100 ) "Initializing" ]
+
+        Initialized (Ok _) ->
+            Html.text "Initialized"
+
+        Initialized (Err e) ->
+            Html.text e
+
+        Running game ->
+            Canvas.toHtml
+                canvasSize
+                []
+                (Canvas.shapes [ Canvas.Settings.fill Color.black ]
+                    [ let
+                        ( w, h ) =
+                            canvasSize
+                      in
+                      Canvas.rect ( 0, 0 ) w h
+                    ]
+                    :: renderItems game
+                )
 
 
-renderItems : Model -> List Canvas.Renderable
-renderItems model =
-    (case model.shipTexture of
-        Failure ->
-            Canvas.shapes [] [ Canvas.rect ( 0, 0 ) 0 0 ]
-
-        Success s ->
-            let
-                ( w, _ ) =
-                    canvasSize
-            in
-            Canvas.texture [] ( 0.5 * w + model.ship.x - 0.5 * model.ship.width, model.ship.y ) s
-
-        Loading ->
-            Canvas.shapes [] [ Canvas.rect ( 0, 0 ) 0 0 ]
+renderItems : Game -> List Canvas.Renderable
+renderItems game =
+    (let
+        ( w, _ ) =
+            canvasSize
+     in
+     Canvas.texture [] ( 0.5 * w + game.ship.x - 0.5 * game.ship.width, game.ship.y ) game.env.shipTexture
     )
-        :: fireShapes model
+        :: fireShapes game
 
 
-fireShapes : Model -> List Canvas.Renderable
-fireShapes model =
+fireShapes : Game -> List Canvas.Renderable
+fireShapes game =
     let
         ( w, _ ) =
             canvasSize
@@ -388,33 +496,30 @@ fireShapes model =
         ( fw, _ ) =
             fireSize
     in
-    model.fires
-        |> List.map
-            (\f ->
-                case model.fireTexture of
-                    Failure ->
-                        Canvas.shapes [] [ Canvas.rect ( 0, 0 ) 0 0 ]
-
-                    Success s ->
-                        Canvas.texture [] ( 0.5 * w + Tuple.first f - 0.5 * fw, Tuple.second f ) s
-
-                    Loading ->
-                        Canvas.shapes [] [ Canvas.rect ( 0, 0 ) 0 0 ]
-            )
+    game.fires
+        |> List.map (\f -> Canvas.texture [] ( 0.5 * w + Tuple.first f - 0.5 * fw, Tuple.second f ) game.env.fireTexture)
 
 
-step : Random.Generator a -> Model -> ( a, Model )
-step generator model =
+step : Random.Generator a -> Env -> ( a, Env )
+step generator env =
     let
         ( a, nextSeed ) =
-            model.seed |> Random.step generator
+            env.seed |> Random.step generator
     in
-    ( a, { model | seed = nextSeed } )
+    ( a, { env | seed = nextSeed } )
 
 
 subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.batch
-        [ Browser.Events.onAnimationFrameDelta Tick
-        , Sub.map KeyMsg Keyboard.subscriptions
-        ]
+subscriptions model =
+    case model of
+        Initialized _ ->
+            Sub.map KeyMsg Keyboard.subscriptions
+
+        Running _ ->
+            Sub.batch
+                [ Browser.Events.onAnimationFrameDelta Tick
+                , Sub.map KeyMsg Keyboard.subscriptions
+                ]
+
+        _ ->
+            Sub.none
